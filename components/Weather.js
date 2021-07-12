7ac67d3fd290264790bb2f8b2446e15a2d354486
6210110377 <6210110377@psu.ac.th>
2021-07-12 12:05:29 +0700

    Flex Box

--- components/Weather.js
+++ components/Weather.js
@@ -4,15 +4,14 @@ import Forecast from './Forecast'
 
 export default function Weather(props){
     const [forecastInfo, setForecastInfo] = useState({
-        main: '-',
-        description: '-',
+        main: 'main',
+        description: 'description', 
         temp: 0
     }) 
     return (
         <View>
              <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
-             <Text>Zip Code</Text>
-             <Text>{props.zipCode}</Text>
+             <Text>Zip Code is {props.zipCode} </Text>
              <Forecast {...forecastInfo} />
         </ImageBackground>
         </View>
@@ -21,6 +20,8 @@ export default function Weather(props){
     }
     const styles = StyleSheet.create({
         backdrop: {
+            flexDirection: 'column',
+            justifyContent: 'flex-start',
             alignItems: 'center',
             width: '100%',
             height: '100%'
